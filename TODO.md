d3d80e59d6cc2ca2aab2a7b597699c599c5c3632
fixedOtter <fo1152rc@go.minnstate.edu>
2023-08-31 23:39:56 -0500

    init - good start after so long but now sleep

---
+++ TODO.md
@@ -0,0 +1,32 @@
+# things that must be done
+trying to break this down quite a bit so i can follow this list when i'm back in the future
+
+## init setup
+### server init
+- [x] touch .gitignore
+- [x] touch server.js
+- [ ] create routes dir
+- [ ] touch routes js
+- [ ] configure node and install dependancies
+- [ ] config nodemon (ignore public folder && database folder) 
+- [ ] touch dummy database
+
+### public folder
+- [X] create public folder
+- [X] touch index.html
+- [X] create css dir
+- [X] touch style.css
+- [X] create js dir
+- [X] touch script.js
+
+## actually writing
+### server writing
+- [ ] write server.js (look to past projects for assist)
+- [ ] 
+- [ ]
+- [ ]
+- [ ]
+
+
+## more todo 
+- [ ] recall what else there is to do (there's always more)
---
+++ server.js
@@ -0,0 +1,42 @@
+//
+// made by fixedOtter on 31.08.2023
+//
+
+/* 
+  defs and stuf
+*/
+
+/* imported goods */ 
+const Express = require('express');
+const path = require('path');
+
+/* routes */
+const crud_routes = require('./routes/crud-routes');
+
+// so i dont keep typing express a billion times
+const app = Express();
+
+// grab port from environment variables or setting to 6969
+const pOrT = process.env.port || 6969;
+
+
+/* 
+  giving stuff to app
+*/
+
+// give end user access to public folder
+app.use(Express.static(path.join(__dirname, 'public')));
+// allow me to pass more values than just strings
+app.use(Express.urlencoded({ extended: true }));
+// allow us to pass json data to backend 
+app.use(Express.json());
+// linking routes to root
+app.use('/api', crud_routes);
+
+
+/* 
+  actually starting node server
+*/
+app.listen(pOrT, () => {
+  console.log(`i can smell you on: ${pOrT}`);
+});
